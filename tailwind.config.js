4977f07727fe28db3e257161e60717efe6ceaf30
deJames-13 <de.james013@gmail.com>
2024-02-08 00:11:34 +0800

    Init Commit

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./resources/**/*.html'],
+  variants: {
+    borderColor: ['responsive', 'hover', 'focus', 'focus-within'],
+  },
+
+  theme: {
+    extend: {
+      transformOrigin: {
+        0: '0%',
+      },
+      zIndex: {
+        '-1': '-1',
+      },
+      colors: {
+        primary: '#A2C2E6',
+        secondary: '#E577A1',
+        cpink: '#E577A1',
+        lightPink: '#eddfd5',
+        cblue: '#A2C2E6',
+        cwhite: '#F6F4F3',
+        cblack: '#080406',
+        cbrown: '#31241e',
+        textSecondary: '#807792',
+        inputBg: '#d1c8c1',
+      },
+    },
+
+    fontFamily: {
+      sans: ['Monsterrat', 'system-ui'],
+      serif: ['ui-serif', 'Georgia'],
+      mono: ['ui-monospace', 'SFMono-Regular'],
+      body: ['"Monsterrat"'],
+      BobbyJones: 'bobbyjones',
+    },
+  },
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    themes: ['cupcake'],
+    base: true,
+    styled: true,
+    utils: true,
+    logs: true,
+    prefix: '',
+  },
+};
